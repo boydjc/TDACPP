34c9a1ffc256a899ac28d59db001d493635220b5
Joshua Boyd <jaboydfamily@gmail.com>
2022-05-06 14:12:44 -0400

    added documentation for setHistPrice function

--- include/TDA.h
+++ include/TDA.h
@@ -21,6 +21,59 @@ class TDA {
 
 		/* BEGIN set & get functions */
 
+		
+		/*  setHistPrice()
+			
+			Gets the historic price for a given ticker
+
+			params:
+
+			ticker - ticker symbol of company
+
+			periodType - The type of period to show. Valid values are day, month, year, or ytd (year to date). 
+						 Default is day.
+
+			period - The number of periods to show.
+
+					 Example: For a 2 day / 1 min chart, the values would be:
+
+					 period: 2
+					 periodType: day
+					 frequency: 1
+					 frequencyType: min
+
+					 Valid periods by periodType (defaults marked with an asterisk):
+
+					 day: 1, 2, 3, 4, 5, 10*
+					 month: 1*, 2, 3, 6
+					 year: 1*, 2, 3, 5, 10, 15, 20
+					 ytd: 1*
+
+			freqType - The type of frequency with which a new candle is formed.
+
+					   Valid frequencyTypes by periodType (defaults marked with an asterisk):
+
+					   day: minute*
+					   month: daily, weekly*
+					   year: daily, weekly, monthly*
+					   ytd: daily, weekly*
+
+			freq - The number of the frequencyType to be included in each candle.
+
+				   Valid frequencies by frequencyType (defaults marked with an asterisk):
+
+				   minute: 1*, 5, 10, 15, 30
+				   daily: 1*
+				   weekly: 1*
+				   monthly: 1*
+
+			endDate - End date as milliseconds since epoch. If startDate and endDate are provided, period should not be provided. 
+					  Default is previous trading day.
+
+			startDate - Start date as milliseconds since epoch. If startDate and endDate are provided, period should not be provided.
+
+			extHourData - true to return extended hours data, false for regular market hours only. Default is true */
+
 		void setHistPrice(std::string ticker, std::string periodType="day",
 					  	  std::string period="", std::string freqType="",
 					  	  std::string freq="", unsigned int endDate=0,
